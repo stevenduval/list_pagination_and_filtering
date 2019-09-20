b0366b1e4da6aee80d6dcddbc8396c1ec8ea51ac
Steven Duval <stevenduval@gmail.com>
2019-09-20 18:22:16 -0500

    test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /******************************************
 Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
+FSJS project 2 - List Filter and Pagination.
 ******************************************/
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
